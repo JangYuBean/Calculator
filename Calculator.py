f54d7f25b47aebc27e610f086f22cc9dbd0fadcb
JangYuBean <hello.git.yubean12@daum.net>
2024-12-10 22:08:18 +0900

    fix: add exponent fuctionality

--- Calculator.py
+++ Calculator.py
@@ -4,6 +4,15 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide (a, b):
+    return a / b
+
+def exponent(base, exp):
+    return base ** exp
+
 def main():
     print("----------------------------")
     print("계산기 프로그램 입니다.")
@@ -12,6 +21,9 @@ def main():
         print("\n사용할 기능을 선택하세요. 종료하려면 'quit'를 입력하세요.")
         print("1. 더하기")
         print("2. 빼기")
+        print("3. 곱하기")
+        print("4. 나누기")
+        print("5. 지수 계산 (x^y)")
 
         choice = input("번호 : ")
 
@@ -26,6 +38,19 @@ def main():
             x = float(input("첫 번째 숫자 입력: "))
             y = float(input("두 번째 숫자 입력: "))
             print(f"결과: {x} - {y} = {subtract(x, y)}")
+        elif choice == "3":
+            x = float(input("첫 번째 숫자 입력: "))
+            y = float(input("두 번째 숫자 입력: "))
+            print(f"결과: {x} - {y} = {multiply(x, y)}")
+        elif choice == "4":
+            x = float(input("첫 번째 숫자 입력: "))
+            y = float(input("두 번째 숫자 입력: "))
+            if y != 0:
+                print(f"결과: {x} / {y} = {divide(x, y)}")
+        elif choice == "5":
+            base = float(input("밑(base) 값을 입력하세요: "))
+            exp = float(input("지수(exponent) 값을 입력하세요: "))
+            print(f"결과: {base}^{exp} = {exponent(base, exp)}")
         else:
             print("잘못된 입력입니다. 다시 시도해주세요.")
 
